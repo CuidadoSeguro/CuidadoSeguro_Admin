e05deaf6739ba26e2674d27cb45c851c5d94e195
eapfgws-dotcom <edua.pastene@duocuc.cl>
2026-09-09 09:17:22 -0300

    WIP0002 Se modifica el controller
    Se modifica el controller para uso de parte del front-end.

--- src/main/java/cl/duoc/msalapi/controller/MeController.java
+++ src/main/java/cl/duoc/msalapi/controller/MeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MeController {
 
-	@GetMapping("/me")
+	@GetMapping("/login")
 	public Map<String, Object> me(@AuthenticationPrincipal Jwt jwt) {
 		Map<String, Object> body = new LinkedHashMap<>();
 		body.put("mensaje", "API privada: el access token es válido.");
